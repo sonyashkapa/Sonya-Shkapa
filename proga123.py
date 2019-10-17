e965f06cc184ad1e30aabdfaac333c9ca4b22814
sonyashkapa <56366295+sonyashkapa@users.noreply.github.com>
2019-10-17 09:57:16 +0300

    Update proga123.py

--- proga123.py
+++ proga123.py
@@ -1,11 +1,17 @@
+"""It is docstring of the module."""
+
 def massiv(str):
+    
+    
     """ Function gets a string on input,
     and it gives an array of
     alphabetical chains as an output """
-    # set the array
+    
+    
+    # set the array, in which each element will be the alphabetical chain 
     data = []  
     # cycle that goes through each character in a string
-    for i,c in enumerate(str):
+    for i, c in enumerate(str):
         # checking if the character is an alphabetical char
         if str[i].isalpha():
             # if the character is an alphabetical char, then we check it's index and the previous element 
