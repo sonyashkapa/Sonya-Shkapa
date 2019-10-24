38ac2a393886e34179ca6d9cc42ecbb980b0b0d7
sonyashkapa <56366295+sonyashkapa@users.noreply.github.com>
2019-10-24 09:51:01 +0300

    Update unittest_with_comments.py

--- unittest_with_comments.py
+++ unittest_with_comments.py
@@ -1,19 +1,19 @@
 import unittest
-# testing the program. for this we call the testcase class
+#  we call the testcase class
 class making_arrayTest(unittest.TestCase):
-    # split breaks a string into parts using a delimiter
+    # "split" breaks a string into parts using a delimiter
     # and returns these parts in a list.
     # so here we check if our program can split the string
     def test_words_split(self):
         s = 'мама мыла раму'
         self.assertEqual(s.split(), ['мама', 'мыла', 'раму'])
-    # isalnum returns a nonzero value if its argument is either a letter
+    # "isalnum" returns a nonzero value if its argument is either a letter
     # of the alphabet (upper or lower case), or a digit   
     # so here we check if our program can distinguish characters from letters
     def test_isalnum(self):
         self.assertTrue('мама23мыла34раму65'.isalnum())
         self.assertFalse('мама!помой*раму3'.isalnum())
-    # isalpha returns a nonzero value if its argument is
+    # "isalpha" returns a nonzero value if its argument is
     # a letter of the alphabet (upper or lower case). 
     # so here we check if our program can distinguish characters from letters
     def test_isanalpha(self):
