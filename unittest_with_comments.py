958121975ea4f32c31c739f0f0a7a27a806a7aa2
sonyashkapa <56366295+sonyashkapa@users.noreply.github.com>
2019-10-24 09:47:48 +0300

    Update unittest_with_comments.py

--- unittest_with_comments.py
+++ unittest_with_comments.py
@@ -1,18 +1,4 @@
 import unittest
-def making_array(str):
-    data = []      
-    for i, c in enumerate(str):        
-        if str[i].isalpha():            
-            if (i > 0) and (str[i-1].isalpha()):
-                continue
-            else:                
-                nomer1 = i
-        else:            
-            if (i > 0) and (str[i-1].isalpha()):               
-                data.append(str[nomer1:i])
-    if str[i].isalpha():
-        data.append(str[nomer1:i+1])
-    return(data)
 # testing the program. for this we call the testcase class
 class making_arrayTest(unittest.TestCase):
     def test(self):
