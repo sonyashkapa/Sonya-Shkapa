75479632899c11cbeca60f1782742f77c1754128
sonyashkapa <56366295+sonyashkapa@users.noreply.github.com>
2019-11-07 10:03:16 +0300

    Update testTokenizator.py

--- testTokenizator.py
+++ testTokenizator.py
@@ -5,24 +5,31 @@ class MakingArrayTest(unittest.TestCase):
         
     def setUp(self):
         self.Tokenizator = Tokenizator()
+    # "split" breaks a string into parts using a delimiter
+    # and returns these parts in a list.
+    # so here we check if our program can split the string
     def test_words_split(self):
         s = 'мама мыла раму'
         self.assertEqual(self.Tokenizator.making_array(s), ['мама','мыла','раму'])
-
+    # "isalnum" returns a nonzero value if its argument is either a letter
+    # of the alphabet (upper or lower case), or a digit   
+    # so here we check if our program can distinguish characters from letters
     def test_isalnum(self):
         s = 'а233465'
         self.assertEqual(self.Tokenizator.making_array(s), ['а'])
-
+    # "isalpha" returns a nonzero value if its argument is
+    # a letter of the alphabet (upper or lower case). 
+    # so here we check if our program can distinguish characters from letters
     def test_isanalpha(self):
         s = 'Мамамылараму'  
         self.assertEqual(self.Tokenizator.making_array(s), ['Мамамылараму'])
         s = '7574мама 00мыла 778раму'
         self.assertEqual(self.Tokenizator.making_array(s), ['мама','мыла','раму'])
-   
+    # program check if an empty string is entered
     def test_empty_string(self):
         s = ''
         self.assertEqual(self.Tokenizator.making_array(s), [])
-
+# do not forget to insert this module into the code you are checking
 if __name__== '__main__':
     unittest.main()
 
