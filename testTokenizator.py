cfee8dab5055991cd88fed4d2dc7fc19dd719363
sonyashkapa <56366295+sonyashkapa@users.noreply.github.com>
2019-11-14 09:57:29 +0300

    Update testTokenizator.py

--- testTokenizator.py
+++ testTokenizator.py
@@ -26,22 +26,20 @@ class MakingArrayTest(unittest.TestCase):
         self.assertEqual(self.tokentest.tokenize(s), [])
 
 class TestUnittest(unittest.TestCase):
-
     def test_isalnum(self):
-            s = 'мама23мыла34раму65'
-            self.assertEqual(tokenizator.making_array(s), ['мама','мыла','раму'])
-            s = 'мама!помой*'
-            self.assertEqual(tokenizator.making_array(s), ['мама','помой'])
+        s = 'мама23мыла34раму65'
+        self.assertEqual(tokenizator.making_array(s), ['мама','мыла','раму'])
+        s = 'мама!помой*'
+        self.assertEqual(tokenizator.making_array(s), ['мама','помой'])
 
-      def test_isanalpha(self):
-            s = 'Мамамылараму'
-            self.assertEqual(tokenizator.making_array(s), ['мамамылараму'])
-            s ='7574мама00мыла778раму'
-            self.assertEqual(tokenizator.making_array(s), ['мама','мыла','раму'])
-
-      def test_empty_string(self):
-            s = ''
-            self.assertEqual(tokenizator.making_array(s), [])
+    def test_isanalpha(self):
+         s = 'Мамамылараму'
+         self.assertEqual(tokenizator.making_array(s), ['мамамылараму'])
+         s ='7574мама00мыла778раму'
+         self.assertEqual(tokenizator.making_array(s), ['мама','мыла','раму'])
+     def test_empty_string(self):
+         s = ''
+         self.assertEqual(tokenizator.making_array(s), [])
        
 
 if __name__== '__main__':
