2cf50202858f7b381c72b2c44d6ab368ad155d6c
sonyashkapa <56366295+sonyashkapa@users.noreply.github.com>
2019-11-28 10:15:35 +0300

    Update new.py

--- new.py
+++ new.py
@@ -2,7 +2,7 @@ def making_array(str):
     i = 0
     letters = []
     numbers = []
-    spaces = 0
+    spaces = []
     punct = []
     if str == '': 
         return []
@@ -12,19 +12,39 @@ def making_array(str):
                 continue
             else:
                 nomer1 = i
+                pretype = alpha
         else:
-            if str[i].isspace():
-                spaces = spaces+1
-            else:
-                if str[i].isdigit():
-                    numbers.append(str[i])
-                else:
-                    punct.append(str[i])
-        
             if (i > 0) and (str[i-1].isalpha()):
-                letters.append(str[nomer1:i])
+                data.append(str[nomer1:i])
+        
+        if str[i].isdigit():
+            if (i > 0) and (str[i-1].isdigit()):
+                continue
+            else:
+                nomer1 = i
+                pretype = digit
+        else:
+            if (i > 0) and (str[i-1].isdigit()):
+                numbers.append(str[nomer1:i])
+        if str[i].isspace():isalpha
+            if (i > 0) and (str[i-1].isspace()):
+                continue
+            else:
+                nomer1 = i
+                pretype = space
+        else:
+            if (i > 0) and (str[i-1].isspace()):
+                spaces.append(str[nomer1:i])
+        if str[i].isalpha():
+            if (i > 0) and (str[i-1].isalpha()):
+                continue
+            else:
+                nomer1 = i
+        else:
+            if (i > 0) and (str[i-1].isalpha()):
+                data.append(str[nomer1:i])
     if str[i].isalpha():
-        letters.append(str[nomer1:i+1])
-    return(letters)
+        data.append(str[nomer1:i+1])
+    return(data)
 s = input
 print(making_array(s))
