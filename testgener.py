d8eaee4b034336760e4f0c753b591dd1ba5f2ccc
sonyashkapa <56366295+sonyashkapa@users.noreply.github.com>
2020-03-02 11:41:35 +0300

    Update and rename testnewtokenizator.py to testgener.py

--- testgener.py
+++ testgener.py
@@ -1,7 +1,8 @@
+
 import unittest
-import newtokenizer
-from newtokenizer import Tokenizator
-from newtokenizer import Token
+import gener
+from gener import Tokenizator
+from gener import Token
 
 
 class TestTokenizatorOfCategories(unittest.TestCase):  
@@ -58,20 +59,42 @@ class TestUnittest(unittest.TestCase):
  
     def test_isalnum(self):
         s = 'мама23мыла34раму65'
-        self.assertEqual(newtokenizer.making_array(s), ['мама','мыла','раму'])
+        self.assertEqual(gener.making_array(s), ['мама','мыла','раму'])
         s = 'мама!помой*'
-        self.assertEqual(newtokenizer.making_array(s), ['мама','помой'])
+        self.assertEqual(gener.making_array(s), ['мама','помой'])
 
     def test_isanalpha(self):
         s = 'Мамамылараму'
-        self.assertEqual(newtokenizer.making_array(s), ['Мамамылараму'])
+        self.assertEqual(gener.making_array(s), ['Мамамылараму'])
         s ='7574мама00мыла778раму'
-        self.assertEqual(newtokenizer.making_array(s), ['мама','мыла','раму'])
+        self.assertEqual(gener.making_array(s), ['мама','мыла','раму'])
 
     def test_empty_string(self):
         s = ''
-        self.assertEqual(newtokenizer.making_array(s), [])
+        self.assertEqual(gener.making_array(s), [])
         
-
+class GeneratorTest(unittest.TestCase):
+        
+    def test_alpha(self):
+        gen = list(Tokenizator.gen_tokenize_cat('1мама мыла рому'))
+        self.assertEqual(gen[1].token, 'мама')
+        self.assertEqual(gen[1].category, 'alpha')
+        
+    def test_space(self):
+        gen = list(Tokenizator.gen_tokenize_cat('мамамыларому'))
+        self.assertEqual(gen[0].token, 'мамамыларому')
+        
+    def test_digit(self):
+        gen = list(Tokenizator.gen_tokenize_cat('012345'))
+        self.assertEqual(gen[0].token,'012345')
+        self.assertEqual(gen[0].category,'digit')
+        self.assertEqual(gen[0].firstindex, 0)
+        self.assertEqual(gen[0].lastindex, 6)
+        
+    def test_nonapha(self):
+        gen = list(Tokenizator.gen_tokenize_cat('мама123мыла'))
+        self.assertEqual(gen[1].token,'123')
+        self.assertEqual(gen[1].category,'digit')
+        
 if __name__ == '__main__':
     unittest.main()
