4340ab09d3b846c37c2124ae7b25ef4e409c3235
sonyashkapa <56366295+sonyashkapa@users.noreply.github.com>
2020-03-24 18:08:17 +0300

    Create log-likelihood.py

---
+++ log-likelihood.py
@@ -0,0 +1,12 @@
+import math
+a = float(input("Введите частоту подкорпуса:"))
+b = float(input("Введите частоту других текстов:"))
+c = float(input("Введите объем подкорпуса:"))
+d = float(input("Введите объем других текстов:"))
+e1 = c * ((a+b)/(c+d))
+e2 = d * ((a+b)/(c+d))
+print("E1 = ",e1,", E2 = ",e2)
+g = a/e1
+g2 = b/e2
+g_in_sqr = 2 * (a* math.log1p(g) + b*math.log1p(g2))
+print("Log-likelyhood = ",g_in_sqr)
